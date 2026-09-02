6c14e7637440ef47326a050e488a2d6ba9667e51
beilinet <1704069611@qq.com>
2026-09-02 20:48:21 +0800

    fix: make Vercel migration build executable

--- migrate.sh
+++ migrate.sh
@@ -1,3 +1,6 @@
-pip install -r requirements.txt
+#!/usr/bin/env bash
+set -euo pipefail
+
+python3 -m pip install --disable-pip-version-check -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
