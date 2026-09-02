69d5410cac08d4a21fdbd2ea7eb45afaa1ed3c65
beilinet <1704069611@qq.com>
2026-09-02 20:52:13 +0800

    fix: install build dependencies with uv

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-python3 -m pip install --disable-pip-version-check -r requirements.txt
+uv pip install --system -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
